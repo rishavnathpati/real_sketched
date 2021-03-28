34772280c89b252a47149bd11b254111af7b33be
rishavnathpati <patirishavnath@gmail.com>
2021-03-28 22:46:22 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/real_sketched/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.real_sketched
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _loading = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromRGBO(138, 35, 138, 1.0),
+                  Color.fromRGBO(233, 64, 87, 1.0),
+                  Color.fromRGBO(242, 113, 33, 1.0)
+                ],
+              ),
+            ),
+          ),
+          _loading==true?Center(
+            child:Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment:MainAxisAlignment.center,
+              children: <Widget>[
+                
+              ],)
+          )
+        ],
+      ),
+    );
+  }
+}
